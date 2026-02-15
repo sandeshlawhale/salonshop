ee98ebd12b38de2eaa87798710a32933d6373a96
Sandesh <sandeshlawhale@gmail.com>
2026-02-15 11:59:43 +0530

    Sandesh | Feat: updated the admin sidebar UI

--- Salon_E_Comm/src/components/admin/AdminLayout.jsx
+++ Salon_E_Comm/src/components/admin/AdminLayout.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import AdminSidebar from './AdminSidebar';
 import AdminHeader from './AdminHeader';
+import { TooltipProvider } from '../ui/tooltip';
 
 export default function AdminLayout({ children, title = 'Dashboard' }) {
     return (
-        <div className="flex min-h-screen bg-neutral-50/50">
-            <AdminSidebar />
-            <div className="flex-1 ml-64 flex flex-col">
-                <AdminHeader title={title} />
-                <main className="p-8">
-                    {children}
-                </main>
+        <TooltipProvider>
+            <div className="flex min-h-screen bg-neutral-50/50">
+                <AdminSidebar />
+                <div className="flex-1 ml-64 flex flex-col">
+                    <AdminHeader title={title} />
+                    <main className="p-8">
+                        {children}
+                    </main>
+                </div>
             </div>
-        </div>
+        </TooltipProvider>
     );
 }
